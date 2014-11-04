abf4523ef6fde1508c07130357bdd0caf3cda7d1
David-S-Hunsicker <david.s.hunsicker@gmail.com>
2014-11-03 23:37:42 -0500

    na

--- blockbreak.js
+++ blockbreak.js
@@ -28,8 +28,8 @@ $(function() {
       } else if(Q.inputs['right']) {
         this.p.x += dt * this.p.speed;
       }
-      if(this.p.x < 0) { 
-        this.p.x = 0+this.p.w/2;//trying to fix this
+      if(this.p.x < 20) { 
+        this.p.x = 20;//trying to fix this
       } else if(this.p.x > Q.width - this.p.w) { 
         this.p.x = Q.width - this.p.w;
       }
